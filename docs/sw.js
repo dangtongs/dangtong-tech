d479ea1cdfbd7e87083a4080d03e098e958ad861
dangtong76 <dangtong@gmail.com>
2022-09-22 02:14:10 +0900

    add kind contents

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/dangtong-tech/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220922.021041';
+const cacheName = 'chirpy-20220922.021357';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
