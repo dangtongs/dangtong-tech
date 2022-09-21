3e121a7943a45bc9824ac325956d3713b045e077
dangtong76 <dangtong@gmail.com>
2022-09-22 01:44:50 +0900

    change photo

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/dangtong-tech/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220922.014420';
+const cacheName = 'chirpy-20220922.014448';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
