0b274dbd45485f909e5f80654582d2cdaa389e95
dangtong76 <dangtong@gmail.com>
2022-09-22 00:15:15 +0900

    change _config

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/dangtong-tech/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220922.001353';
+const cacheName = 'chirpy-20220922.001506';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
